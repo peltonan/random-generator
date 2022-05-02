029dd73699f52827b094620a86ce62d580160e79
peltonan <peltonen.annette@gmail.com>
2022-05-02 12:46:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="projectTitle">Feel Better Quotes</h1>
+        <p className="quote">Quote appears here!</p>
+        <div className="generateBtn">
+          <button>Generate</button>
+        </div>
+      </header>
+      <footer>Created by A. Peltonen</footer>
+    </div>
+  );
+}
+
+export default App;
